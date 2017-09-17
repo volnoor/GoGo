4ccc1da67779ef569c9677a419812adb4adaf297
Eugene <volnoor@gmail.com>
2017-09-17 21:07:59 +0300

    done

--- app/src/main/java/com/volnoor/gogo/MyFirebaseMessagingService.java
+++ app/src/main/java/com/volnoor/gogo/MyFirebaseMessagingService.java
@@ -1,18 +1,10 @@
 package com.volnoor.gogo;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 
 /**
  * Created by Eugene on 16.09.2017.
@@ -20,38 +12,7 @@ import java.net.URLDecoder;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        System.out.println("onMessageReceived" + remoteMessage.getData());
-//
-//        Intent intent = new Intent(this, MainActivity.class);
-//        intent.putExtra("userId", remoteMessage.getData().get("userId"));
-//        intent.putExtra("changesCount", remoteMessage.getData().get("changesCount"));
-//
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-//
-//        NotificationCompat.Builder notificationBuilder = null;
-//
-//        try {
-//            notificationBuilder = new NotificationCompat.Builder(this)
-//                    .setSmallIcon(R.mipmap.ic_launcher)
-//                    .setContentTitle(URLDecoder.decode(remoteMessage.getNotification().getTitle(), "UTF-8"))
-//                    .setContentText(URLDecoder.decode(remoteMessage.getNotification().getBody(), "UTF-8"))
-//                    .setAutoCancel(true)
-//                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-//                    .setContentIntent(pendingIntent);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        if (notificationBuilder != null) {
-//            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//            notificationManager.notify(0, notificationBuilder.build());
-//        }
-//    }
-    //
-private LocalBroadcastManager broadcaster;
+    private LocalBroadcastManager broadcaster;
 
     @Override
     public void onCreate() {
--- app/src/main/java/com/volnoor/gogo/RepoActivity.java
+++ app/src/main/java/com/volnoor/gogo/RepoActivity.java
@@ -1,12 +1,11 @@
 package com.volnoor.gogo;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
